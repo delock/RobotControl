255d998b86fb668fad7d50ff53fb9fe8d2a3662e
Ma, Guokai <guokai.ma@gmail.com>
2017-08-13 18:44:15 +0800

    reduce the size of payload to improve performance

--- robot_control.py
+++ robot_control.py
@@ -4,14 +4,16 @@ import cv2
 import pickle
 import struct
 import numpy as np
+import time
 
 # create connection
 conn=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-conn.connect(('192.168.0.114',8089))
+conn.connect(('192.168.0.114',8086))
 
 data = ""
-payload_size = struct.calcsize("L") 
+payload_size = struct.calcsize("L")
 
+old_time = time.time()
 while True:
     while len(data) < payload_size:
         data += conn.recv(4096)
@@ -25,8 +27,13 @@ while True:
 
     compressed_frame = pickle.loads(frame_data)
     frame = cv2.imdecode(compressed_frame, 1)
+    #big_frame = cv2.resize(frame, (1920, 1080), interpolation=cv2.INTER_CUBIC)
     cv2.imshow('frame', frame)
     k = cv2.waitKey(1)
     if k == 27:
         cv2.destoryWindow()
         break
+    cur_time = time.time()
+    fps = 1/(cur_time - old_time)
+    print ("fps = " + str(fps))
+    old_time = cur_time
