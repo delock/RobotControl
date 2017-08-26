e8a52b6a7236229a7942f9608e9b4d771d6f143a
Ma Guokai <guokai.ma@gmail.com>
2017-08-26 23:29:15 +0800

    add bottom half directory
    Former-commit-id: c5170219e07361659649e9cb978edab509596563

---
+++ bottom_half/MotorTest/MotorTest.ino
@@ -0,0 +1,41 @@
+/****************************************************
+ * MotorTest
+ *****************************************************
+ * This example is aimed to control DFMobile basic motion -- Forward and Back
+ * Created 2015-3-2
+ * By Gavin
+ ******************************************************/
+#include <DFMobile.h>
+
+DFMobile Robot (4,5,7,6);     // initiate the Motor pin
+
+void setup () {
+  Robot.Direction (HIGH, LOW);  // initiate the positive direction  
+}
+
+#define SPEED 255
+void loop () {
+  Robot.Speed (SPEED, SPEED);      //Forward
+  delay (1000);
+
+  Robot.Speed (-SPEED,-SPEED);    //Back
+  delay (1000);
+
+  Robot.Speed (SPEED,-SPEED);      //Forward
+  delay (1000);
+
+  Robot.Speed (-SPEED,SPEED);    //Back
+  delay (1000);
+  
+  Robot.Speed (0, 0);     //Stop
+  delay (2000);
+
+}
+
+
+
+
+
+
+
+
