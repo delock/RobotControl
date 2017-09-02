1d71fc7668d2d198d43397381d0f4113754e3f92
Ma Guokai <guokai.ma@gmail.com>
2017-09-02 16:41:22 +0800

    command interface work in position, now camera pitch/yaw can be set from serial command
    Former-commit-id: 317003924ddc5541222345138acaf4dd4bc1bc18

---
+++ bottom_half/test.py
@@ -0,0 +1,12 @@
+import serial
+import sys
+print (sys.argv[1])
+ser = serial.Serial('/dev/ttyACM0', 9600)
+ser.write(bytes(sys.argv[1], "utf-8"))
+val = ser.readline()
+string = val.decode("utf-8");
+print (string)
+while (string != "+OK\r\n"):
+    val = ser.readline()
+    string = val.decode("utf-8");
+    print (string)
