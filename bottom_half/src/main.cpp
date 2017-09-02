b18ac5bdf1d2c0ab2dbdeb25bb5e987f7d3cb242
Ma Guokai <guokai.ma@gmail.com>
2017-09-02 16:41:22 +0800

    command interface work in position, now camera pitch/yaw can be set from serial command
    Former-commit-id: 616aa68f647226d43af49c30a24018b3e1559d48

--- bottom_half/src/main.cpp
+++ bottom_half/src/main.cpp
@@ -6,6 +6,8 @@
 
 void setup()
 {
+    Serial.begin (9600);
+
     initCamServo();
     camTurnTo (1.0, 0.0);
     delay (1000);
@@ -15,9 +17,41 @@ void setup()
     delay (1000);
     camTurnTo (-0.5, 0.0);
     delay (1000);
-    camTurnTo (0.5, 0.0);
+    camTurnTo (0.0, 0.0);
+    delay (1000);
+    while (!Serial);
+}
+
+void processCommand (String command)
+{
+    if (command.startsWith("campos ")) {
+        float pitch, yaw;
+
+
+        String pitchStr = command.substring(7);
+        int index = pitchStr.indexOf(' ');
+        String yawStr = pitchStr.substring(index+1);
+
+        pitch = pitchStr.toInt()/1000.0;
+        yaw   = yawStr.toInt()/1000.0;
+
+        Serial.print("set camera pos: ");
+        Serial.print(pitch);
+        Serial.print(" , ");
+        Serial.println(yaw);
+        camTurnTo(pitch, yaw);
+    }
 }
 
 void loop()
 {
+    String val;
+    #if 1
+    if (Serial.available()) {
+        val = Serial.readString();
+        processCommand(val);
+        Serial.println("+OK");
+    }
+    #endif
+    delay (1000);
 }
