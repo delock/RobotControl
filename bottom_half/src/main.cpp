dadf1d9620ce2f9af8e422030e13b560209aa3f5
Ma, Guokai <guokai.ma@gmail.com>
2017-09-02 14:38:19 +0800

    servo test
    Former-commit-id: b7f01357c9e08132d7866884897667ae73082764

--- bottom_half/src/main.cpp
+++ bottom_half/src/main.cpp
@@ -5,9 +5,15 @@
 #include <Servo.h>
 
 Servo pitchCam;   // attach your pitch servo to pin 9
+#define PIT_CAM_SERVO_PIN 9
 //Servo yawCam;   // I have no installed this servo yet
 
-void init
+void initCamServo()
+{
+    pitchCam.attach(PITCH_CAM_SERVO_PIN);
+}
+
 void setup()
 {
+    initCamServo();
 }
