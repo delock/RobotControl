d1f549b0afebd06b70a32c6dd5a0e9639a535e07
Ma Guokai <guokai.ma@gmail.com>
2017-09-02 14:35:51 +0800

    adding bottom half code, ongoing
    Former-commit-id: c49396c75abfe8f830682edc28ba6f4008d93dc1

---
+++ bottom_half/src/main.cpp
@@ -0,0 +1,13 @@
+// Bottom half firmware
+// by delock https://github.com/delock
+
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo pitchCam;   // attach your pitch servo to pin 9
+//Servo yawCam;   // I have no installed this servo yet
+
+void init
+void setup()
+{
+}
