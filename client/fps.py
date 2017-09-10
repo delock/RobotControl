a658339951539999dc5b1c9498d4e4c42fe5eee9
Ma, Guokai <guokai.ma@gmail.com>
2017-09-10 16:21:19 +0800

    add average fps support, but still need some fine tuning

---
+++ client/fps.py
@@ -0,0 +1,27 @@
+times = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+time_index = 0
+total_count = 0
+
+def getFPS(time):
+    global times
+    global time_index
+    global total_count
+
+    times[time_index] = time
+    total_count = total_count + 1
+    time_index = time_index + 1
+    if (total_count > 6):
+        total_count = 6
+    if (time_index >= 6):
+        time_index = 0
+
+    total_time = 0.0
+    for i in range(0,total_count):
+        total_time = total_time + times[i]
+
+    avg_time = total_time/total_count
+
+    ret_fps = 1/avg_time
+    ret_fps = int(ret_fps*10)/10
+
+    return ret_fps
