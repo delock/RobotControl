8aa1a5ceddb6decd683044d7d4a6ce2beb8ab222
Ma, Guokai <guokai.ma@gmail.com>
2017-09-13 00:27:32 +0800

    on screen button added, ready to port to android

--- client/robot_control.py
+++ client/robot_control.py
@@ -21,6 +21,7 @@ Config.set('graphics', 'borderless', '1')
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
@@ -129,6 +130,43 @@ class RobotControl(App):
         self.command = "00"
         return True
 
+    light = 0
+    def button_callback(self, instance, value):
+        if (instance == self.button_w and value == "down"):
+            self.command = "W8"
+        elif (instance == self.button_a and value == "down"):
+            self.command = "W4"
+        elif (instance == self.button_s and value == "down"):
+            self.command = "W2"
+        elif (instance == self.button_d and value == "down"):
+            self.command = "W6"
+
+        elif (instance == self.button_left and value == "down"):
+            self.command = "C4"
+        elif (instance == self.button_right and value == "down"):
+            self.command = "C6"
+        elif (instance == self.button_up and value == "down"):
+            self.command = "C8"
+        elif (instance == self.button_down and value == "down"):
+            self.command = "C2"
+
+        elif (instance == self.button_top and value == "down"):
+            self.command = "C0"
+        elif (instance == self.button_front and value == "down"):
+            self.command = "C5"
+
+        elif (instance == self.button_light and value == "down"):
+            self.light = 1-self.light
+            if (self.light == 1):
+                self.command = "L1"
+                instance.text = "#"
+            if (self.light == 0):
+                self.command = "L0"
+                instance.text = "-"
+
+        else:
+            self.command = "00"
+
     def build(self):
         middle_portion = window_height/window_width
         side_portion = (1-middle_portion)/2
@@ -141,6 +179,44 @@ class RobotControl(App):
         self.label_4 = Label(text="rank-4", font_size='25sp', text_size = (side_pix, 25))
         self.label_5 = Label(text="rank-5", font_size='20sp', text_size = (side_pix, 20))
         self.label_fps = Label(text="fps", font_size='40sp', text_size = (side_pix, 40))
+
+        self.button_w = Button(text="W", font_size = '40sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': (side_portion/3), 'y':0.24})
+        self.button_a = Button(text="A", font_size = '40sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': 0, 'y':0.12})
+        self.button_s = Button(text="S", font_size = '40sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': (side_portion/3), 'y':0})
+        self.button_d = Button(text="D", font_size = '40sp', size_hint = (side_portion/4, 0.1),
+
+                                                             pos_hint = {'x': (side_portion*2/3), 'y':0.12})
+        self.button_up = Button(text="^", font_size = '40sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': (1-side_portion)+(side_portion/3), 'y':0.24})
+        self.button_left = Button(text="<", font_size = '40sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': (1-side_portion), 'y':0.12})
+        self.button_down = Button(text="v", font_size = '40sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': (1-side_portion)+(side_portion/3), 'y':0})
+        self.button_right = Button(text=">", font_size = '40sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': (1-side_portion)+(side_portion*2/3), 'y':0.12})
+
+        self.button_top = Button(text="skyview", font_size = '20sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': (1-side_portion)+(side_portion/3), 'y':0.52})
+        self.button_front = Button(text="frontview", font_size = '20sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': (1-side_portion)+(side_portion/3), 'y':0.4})
+
+        self.button_light = Button(text="-", font_size = '40sp', size_hint = (side_portion/4, 0.1),
+                                                             pos_hint = {'x': (side_portion/3), 'y':0.4})
+
+        self.button_w.bind(state=self.button_callback)
+        self.button_a.bind(state=self.button_callback)
+        self.button_s.bind(state=self.button_callback)
+        self.button_d.bind(state=self.button_callback)
+        self.button_up.bind(state=self.button_callback)
+        self.button_down.bind(state=self.button_callback)
+        self.button_left.bind(state=self.button_callback)
+        self.button_right.bind(state=self.button_callback)
+        self.button_top.bind(state=self.button_callback)
+        self.button_front.bind(state=self.button_callback)
+        self.button_light.bind(state=self.button_callback)
 
         self.im = None
         self.camera_feed = CameraFeed(size_hint=(middle_portion, 1.0), pos_hint={'x':side_portion, 'y':0})
@@ -155,6 +231,21 @@ class RobotControl(App):
         top_layout = FloatLayout(size=(1920,1080))
         top_layout.add_widget(self.camera_feed)
         top_layout.add_widget(label_layout)
+
+        top_layout.add_widget(self.button_w)
+        top_layout.add_widget(self.button_a)
+        top_layout.add_widget(self.button_s)
+        top_layout.add_widget(self.button_d)
+
+        top_layout.add_widget(self.button_up)
+        top_layout.add_widget(self.button_down)
+        top_layout.add_widget(self.button_left)
+        top_layout.add_widget(self.button_right)
+
+        top_layout.add_widget(self.button_light)
+
+        top_layout.add_widget(self.button_top)
+        top_layout.add_widget(self.button_front)
 
         return top_layout
 
