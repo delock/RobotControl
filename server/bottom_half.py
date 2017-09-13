c946437bf2248173641be7ef9a6664825e63b41b
Ma Guokai <guokai.ma@gmail.com>
2017-09-13 22:34:21 +0800

    put bottom half command loop in a seperate thread, so the bottom half can be self aware rather than need external connection

--- server/bottom_half.py
+++ server/bottom_half.py
@@ -2,20 +2,45 @@
 import time
 import serial
 import settings as st
+import _thread as thread
 
 def init():
     global ser
+    global command_list
 
     ser = serial.Serial('/dev/ttyACM0', 115200)
+
+    command_list = []
+    thread.start_new_thread(send_command_thread, ("Bottom half thread", (0)))
+
     cam_position(1000, 0)
 
-def send_command(command):
+def send_command_thread(threadName, arg):
     global ser
+
+    try:
+        while True:
+            send_next_command()
+            time.sleep(0.01)
+    finally:
+        close()
+
+def send_next_command():
+    global command_list
+    if (len(command_list) > 0):
+        command = command_list[0]
+        send_command(command)
+        command_list.remove(command)
+        return True
+    else:
+        send_command("nop")
+        return False
+
+def send_command(command):
     ser.write(bytes(command+"\n", "utf-8"))
     while True:
         val = ser.readline()
         string = val.decode("utf-8")
-        #print (string)
         if (string.startswith("+OK")):
             print (string)
             break
@@ -24,7 +49,7 @@ def cam_position(pitch, yaw):
     global cam_pitch
     global cam_yaw
 
-    send_command("campos "
+    command_list.append("campos "
                         + str(pitch) + " "
                         + str(yaw))
     cam_pitch = pitch
@@ -67,7 +92,7 @@ def cam_right(delta = 100):
     cam_position(cam_pitch, cam_yaw)
 
 def wheel_speed (l_speed, r_speed):
-    send_command("wheel " + str(l_speed) + " " + str(r_speed));
+    command_list.append("wheel " + str(l_speed) + " " + str(r_speed));
 
 def wheel_forward(speed = 1000):
     wheel_speed (speed, speed)
@@ -86,12 +111,9 @@ def wheel_stop():
 
 def light(on):
     if (on):
-        send_command("light 1")
+        command_list.append("light 1")
     else:
-        send_command("light 0")
-
-def nop():
-    send_command("nop ")
+        command_list.append("light 0")
 
 def close():
     global ser
