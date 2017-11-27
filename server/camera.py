bb2e9fa686833dccc15e4af0ef4fe74dff40544d
Ma Guokai <guokai.ma@gmail.com>
2017-11-27 23:22:26 +0800

    trim frame to the center

--- server/camera.py
+++ server/camera.py
@@ -4,12 +4,19 @@ import _thread as thread
 import time
 import settings as st
 
+global cap_width
+global cap_height
+cap_width = 1920
+cap_height = 1080
+
 def init():
     global cap
+    global cap_width
+    global cap_height
 
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, cap_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cap_height)
     cap.set(cv2.CAP_PROP_FPS, 30)
 
     _, st.frame = cap.read()
@@ -18,12 +25,18 @@ def init():
     thread.start_new_thread (grab_frame, ("Capture thread", cap))
 
 def grab_frame(threadName, cap):
+    global cap_width
+    global cap_height
     st.frame_index = 0
     try:
         while True:
             #begin = time.time()
             _, cap_frame = cap.read()
-            st.frame = cap_frame[0:1079, 420:1499]
+            blank_width = int((cap_width-cap_height)/2)
+            temp_frame = cap_frame[0:cap_height-1, blank_width:cap_width-blank_width-1]
+            frame_trim = int(cap_height*(1-1/1.5)/2)
+            center_frame = temp_frame[frame_trim:cap_height-1-frame_trim, frame_trim:cap_height-1-frame_trim]
+            st.frame = center_frame
             st.frame_index = st.frame_index + 1
             #end = time.time()
             #print ("capture frame in " + str(end-begin) + " s")
